7f5ae0b99eb8a9b147c9470918cb3f5b50f835b9
Apoz626 <133711643+Apoz626@users.noreply.github.com>
2025-12-30 15:21:18 -0600

    Added try and except to randomNumber

--- randomNumber.py
+++ randomNumber.py
@@ -1,18 +1,41 @@
+##import random
+##
+##def play():
+##    number = random.randint(1, 5)
+##
+##    print("Guess a random number between 1 and 5")
+##    try:
+##        guess = int(input())
+##
+##        while (guess != number):
+##            print("Try again!")
+##            guess = int(input())
+##            
+##        print("Congratulation! You guessed it!")
+##    except ValueError:
+##        print('Error: You must enter a number.')
+##        print('Restarting...\n')
+##        play()
+##
+##play()
+
 import random
 
 def play():
-    guess = ''
-    number = random.randint(1, 5)
-    found = False
+    number = random.randint(1,5)
 
-    print("Guess a random number between 1 and 5")
-    guess = int(input())
+    print("Guess a number between 1 and 5" + str(number))
 
-    while (guess != number):
-        print("Try again!")
-        guess = int(input())
-        
-    print("Congratulation! You guessed it!")
+    while True:
+        try:
+            guess = int(input())
+            if(guess == number):
+                print("Congratulations")
+                break
+            else:
+                print('Try again')
+        except ValueError:
+            print('Error: Must enter a number')
+            continue
 
 play()
-
