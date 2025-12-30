4ee35ee6b243aa708c7eaaa4fe9ea468798d19d7
Apoz626 <133711643+Apoz626@users.noreply.github.com>
2025-12-30 15:01:47 -0600

    Added Try_Except

---
+++ Try_Except.py
@@ -0,0 +1,22 @@
+##def div42by(divideBy):
+##    try:
+##        return 42 / divideBy
+##    except ZeroDivisionError:
+##        print('Error: You tried to divide by zero.')
+##
+##print(div42by(0))
+
+print('How many cats do you have?')
+numCats = input()
+
+try:
+    if int(numCats) >= 4:
+        print('That is a lot of cats.')
+    elif int(numCats) < 0:
+        print('You can not have less than zero cats.')
+    else:
+        print('That is not that many cats')
+        
+except ValueError:
+    print('You did not enter a number.')
+
